fb244e05cc12b774d3fbcf8c16156835b687c185
GabrieleAzzalin <gabriele.azzalin.studenti@isii.it>
2023-03-14 22:29:45 +0100

    grafici

---
+++ grafico5.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<style>
+.card-3{
+    width: 500px;
+    height: 240px;
+    background-color: #DABE83 ;
+    border-radius: 10px;
+    /* mettere le shadow */
+}
+</style>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>All Chart</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+</head>
+<body>
+    <div class="card-3">
+        <span class="class-text-1">Wardrobe</span> <br>
+        <span class="class-text-2">Searched 99k times</span> <br>
+    <canvas id="myChart5" width="400px"></canvas>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+require("grafico1.php");
+?> <br>
+<?php
+require("grafico2.php");
+?> <br>
+<?php
+require("grafico3.php");
+?> <br>
+<?php
+require("grafico4.php");
+?> <br>
+<?php
+require("grafico5.php");
+?> <br>
+<?php
+require("mappa.php");
+?>
