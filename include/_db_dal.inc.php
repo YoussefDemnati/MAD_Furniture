bad018b9cba52cd4d580e00bb3bb882aae99681e
w3schools-test <test@w3schools.com>
2023-03-17 09:45:46 +0100

    inizio backend signup php

--- include/_db_dal.inc.php
+++ include/_db_dal.inc.php
@@ -7,13 +7,25 @@ function db_connect(){
 
     // Verifica connessione
     if ($conn->connect_error) {
-        die("Connction : " . $conn->connect_error);
+        die("Connction failed: " . $conn->connect_error);
     }
     return $conn;
 }
 
 function signup_azienda($company_name, $address, $email, $phone, $password){
     $conn = db_connect();
+    $company_name = mysqli_real_escape_string($conn, $_POST['company_name']);
+    $address = mysqli_real_escape_string($conn, $_POST['address']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $phone = mysqli_real_escape_string($conn, $_POST['phone']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    // Inserimento dei dati nel database con prepared statements
+    $stmt = $conn->prepare("INSERT INTO azienda (nome, indirizzo, email, telefono, password) VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssss", $company_name, $address, $email, $phone, $hashed_password);
+    $stmt->execute();
 }
 function signup_privato(){
     $conn = db_connect();
