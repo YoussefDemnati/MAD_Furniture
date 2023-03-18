6f74e9de73b96de56063066cc3943c7346f4a3e3
nikita <nikita.macreniuc.studenti@isii.it>
2023-03-18 21:27:09 +0100

    layout pagina profile

--- utente_registrato/profile.php
+++ utente_registrato/profile.php
@@ -1,42 +1,44 @@
 <?php include("_header.php") ?>
         <div class="profile">
             <div class="profile-desc-container">
-                <div class="profile-image">image</div>
-                <div class="profile-name"><input type="text" value="Mario Rossi"></div>
-                <div class="profile-email"><input type="text" value="mario.rossi@gmail.com"></div>
+                <div><img class="profile-image" src="../assets/img/profile-image.png" alt="profile image"></div>
+                <div>
+                    <div class="profile-name"><input type="text" value="Mario Rossi"></div>
+                    <div class="profile-email"><input type="text" value="mario.rossi@gmail.com"></div>
+                </div>
                 <div class="address">
                     <label style="display: block;" for="adress">Address</label>
                     <input type="text" placeholder="Address, Street number, City">
                 </div>
                 <div class="province">
                     <select>
-                        <option selected>Select province</option>
+                        <option selected>Province</option>
                         <option>1</option>
                         <option>2</option>
                         <option>3</option>
                     </select>
                 </div>
+                <button class="save-button">Save</button>
             </div>
             <div class="profile-options-container">
-                <div class="cart">
-                    <a href="">Cart</a>
-                </div>
-                <div class="wishlist">
-                    <a href="">Wishlist</a>
-                </div>
-                <div class="orders">
-                    <a href="">My orders</a>
-                </div>
-                <div class="history">
-                    <a href="">History</a>
-                </div>
-                <div class="logout">
-                    <a href="">Logout</a>
-                </div>
-                <div class="delAcc">
-                    <a href="">Delete account</a>
-                </div>
+                <button class="cart option-button">
+                    Cart
+                </button>
+                <button class="wishlist option-button">
+                    Wishlist
+                </button>
+                <button class="orders option-button">
+                    My orders
+                </button>
+                <button class="history option-button">
+                    History
+                </button>
+                <button class="logout option-button">
+                    Logout
+                </button>
+                <button class="delAcc option-button">
+                    Delete account
+                </button>
             </div>
         </div>
-    </body>
-</html>
+<?php include("_footer.php"); ?>
