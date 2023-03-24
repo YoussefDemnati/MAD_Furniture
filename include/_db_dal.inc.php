715856ae1b55d48b6a48ea213c4ca9695fc6ece6
GabrieleAzzalin <gabriele.azzalin.studenti@isii.it>
2023-03-24 09:44:43 +0100

    new_product (finish)

--- include/_db_dal.inc.php
+++ include/_db_dal.inc.php
@@ -1,6 +1,9 @@
 <?php
 require('_db_config.inc.php');
 
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 function db_connect(){
     global $servername, $db_username, $db_password, $db_name;
     $conn = new mysqli($servername, $db_username, $db_password, $db_name);
@@ -27,20 +30,21 @@ function signup_privato(){
     $conn = db_connect();
 }
 
-function new_product($conn,$titolo,$descrizione,$prezzo,$tipo_prodotto,$altezza,$larghezza,$profondita,$spessore,$modello,$casa_produttrice,$indirizzo_magazzino,$forma,$tipo,$categoria,$image){
+function new_product($conn,$titolo,$descrizione,$prezzo,$tipo_prodotto,$altezza,$larghezza,$profondita,$spessore,$modello,$casa_produttrice,$indirizzo_magazzino,$forma,$tipo,$categoria,$azienda,$image){
     $oggi = date('Y-m-d H:i:s');
 
     $main_sql= "INSERT INTO `prodotto` (`id_p`, `titolo`, `descrizione`, `prezzo`,
     `data_inserimento`, `tipo_prodotto_finito`, `altezza`, `larghezza`,
     `profondita`, `spessore`, `modello`, `casa_produttrice`,
-    `indirizzo_magazzino`, `forma`, `tipo`, `hidden`, `id_pc`, `id_cat`)
-    VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, false, NULL, ?)";
+    `indirizzo_magazzino`, `forma`, `tipo`, `hidden`, `id_pc`, `id_cat`, `id_a`)
+    VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, false, NULL, ?, ?)";
 
     $stmt = $conn->prepare($main_sql);
-    $stmt->bind_param("ssdssddddsssssi", $titolo, $descrizione, $prezzo, $oggi,
+    $stmt->bind_param("ssdssddddsssssii", $titolo, $descrizione, $prezzo, $oggi,
     $tipo_prodotto,$altezza,$larghezza,$profondita,$spessore,$modello,
-    $casa_produttrice,$indirizzo_magazzino,$forma,$tipo,$categoria);
+    $casa_produttrice,$indirizzo_magazzino,$forma,$tipo,$categoria, $azienda);
     $stmt->execute();
+    $stmt->close();
 
     $last_element_sql="SELECT * FROM `prodotto` ORDER BY id_p DESC LIMIT 1;";
     $result = $conn->query($last_element_sql);
@@ -49,13 +53,46 @@ function new_product($conn,$titolo,$descrizione,$prezzo,$tipo_prodotto,$altezza,
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
         $ultimo_record = $row;
+        echo "La tabella non è vuota.";
     } else {
         echo "La tabella è vuota.";
     }
 
-    $image_sql= "INSERT INTO `immagine` (`id_img`,`img`, `id_p`) VALUES (NULL,?, ?)";
+    try {
+    $image_sql= "INSERT INTO `immagine` (`id_img`,`img`, `id_p`) VALUES (NULL, ?, ?)";
     $stmt = $conn->prepare($image_sql);
-    $stmt->bind_param("bi",$image,$ultimo_record);
+    $stmt->bind_param("si",$image,$ultimo_record);
+    $stmt->execute();
     $stmt->close();
+} catch (PDOException $e) {
+    // handle the exception here
+    // echo "Error: " . $e->getMessage();
+    die("error .: " . $e->getMessage());
+}
+}
+
+function imagefromblob($conn,$id_p){
+// Query per selezionare l'immagine dal database
+    $sql = "SELECT img FROM immagine WHERE id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i",$id_p);
+    $stmt->execute();
+    $stmt->close();
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+    // Leggi il contenuto del campo BLOB
+    $row = $result->fetch_assoc();
+    $image_data = $row["image_blob"];
+
+    // Imposta l'header della risposta come un'immagine
+    header("Content-Type: image/png");
+
+    // Stampa il contenuto del campo BLOB
+    echo $image_data;
+    } else {
+    echo "Nessuna immagine trovata";
+    }
+
 }
 ?>
--- utente_azienda/new_product.php
+++ utente_azienda/new_product.php
@@ -3,59 +3,65 @@ require('_header.php');
 // require('./include/_db_dal.inc.php');
 require('../include/_db_dal.inc.php');
 $conn = db_connect();
-    if(isset($_POST['titolo'])){
-		if ($_FILES["image"]["error"] == UPLOAD_ERR_OK) {
-			// Otteniamo il percorso del file temporaneo che PHP ha creato quando l'immagine è stata caricata
-			$tmp_name = $_FILES["image"]["tmp_name"];
+		$azienda = 2;
+		//DA SISTEMARE, PRENDERE ID DELL' AZIENDA LOGGATA
+		if(isset($_POST['titolo'])){
+			if ($_FILES["image"]["error"] == UPLOAD_ERR_OK) {
+				// Otteniamo il percorso del file temporaneo che PHP ha creato quando l'immagine è stata caricata
+				$tmp_name = $_FILES["image"]["tmp_name"];
 		
-			// Otteniamo il nome originale del file caricato
-			$name = basename($_FILES["image"]["name"]);
+				// Otteniamo il nome originale del file caricato
+				$name = basename($_FILES["image"]["name"]);
 		
-			// Otteniamo l'estensione del file (ad esempio "png", "jpg", "gif", ecc.)
-			$ext = pathinfo($name, PATHINFO_EXTENSION);
+				// Otteniamo l'estensione del file (ad esempio "png", "jpg", "gif", ecc.)
+				$ext = pathinfo($name, PATHINFO_EXTENSION);
 		
-			// Creiamo un nuovo nome di file per la versione compressa dell'immagine
-			$filename = pathinfo($name, PATHINFO_FILENAME) . '_compressed.' . $ext;
-			
-			// Se l'immagine è in formato PNG
-			if ($ext == 'png') {
-				// Creiamo una nuova immagine PNG dal file temporaneo utilizzando la funzione "imagecreatefrompng()"
-				$png = imagecreatefrompng($tmp_name);
-		
-				// Disabilitiamo la miscelazione di colori per mantenere l'opacità dell'immagine utilizzando le funzioni "imagealphablending()" e "imagesavealpha()"
-				imagealphablending($png, false);
-				imagesavealpha($png, true);
-		
-				// Impostiamo la qualità della compressione PNG (0 = migliore qualità, 9 = peggiore qualità)
-				$quality = 9;
-		
-				// Salviamo la versione compressa dell'immagine PNG utilizzando la funzione "imagepng()"
-				imagepng($png, $filename, $quality);
-		
-				// Liberiamo la memoria utilizzata dalla variabile $png utilizzando la funzione "imagedestroy()"
-				imagedestroy($png);
-			} else {
-				// Se l'immagine non è in formato PNG, spostiamo semplicemente il file temporaneo nel percorso specificato utilizzando la funzione "move_uploaded_file()"
-				move_uploaded_file($tmp_name, $filename);
-			}
-		}
-		
-		
-		// $filename = $_FILES['image'];
-		// $image = imagecreatefrompng($filename);
-		// $imageInfo = getimagesize($filename);
-		// imagepng($image, $filename, 9);
-    	// $blob = 'data:' . $imageInfo['mime'] . ';base64,' . base64_encode($content);
+				// Creiamo un nuovo nome di file per la versione compressa dell'immagine
+				$filename = pathinfo($name, PATHINFO_FILENAME) . '_compressed.' . $ext;
+		// Se l'immagine è in formato PNG
+        if ($ext == 'png') {
+            // Creiamo una nuova immagine PNG dal file temporaneo utilizzando la funzione "imagecreatefrompng()"
+            $png = imagecreatefrompng($tmp_name);
 
-    	new_product($conn,$_POST['titolo'],$_POST['descrizione'],$_POST['prezzo'],
+            // Disabilitiamo la miscelazione di colori per mantenere l'opacità dell'immagine utilizzando le funzioni "imagealphablending()" e "imagesavealpha()"
+            imagealphablending($png, false);
+            imagesavealpha($png, true);
+
+            // Impostiamo la qualità della compressione PNG (0 = migliore qualità, 9 = peggiore qualità)
+            $quality = 9;
+
+            // Salviamo la versione compressa dell'immagine PNG utilizzando la funzione "imagepng()"
+            imagepng($png, $filename, $quality);
+
+            // Liberiamo la memoria utilizzata dalla variabile $png utilizzando la funzione "imagedestroy()"
+            imagedestroy($png);
+        }
+		// Comprimiamo l'immagine finché non raggiunge una dimensione massima di 64 KB
+        $max_size = 64000;
+        while (filesize($filename) > $max_size) {
+            if ($ext == 'png') {
+                // Riduciamo gradualmente la qualità della compressione PNG finché l'immagine non raggiunge la dimensione massima
+                $quality--;
+                imagepng(imagecreatefrompng($filename), $filename, $quality);
+            } else {
+                // Riduciamo gradualmente la qualità della compressione JPEG finché l'immagine non raggiunge la dimensione massima
+                $quality -= 5;
+                if ($quality < 0) $quality = 0;
+                imagejpeg(imagecreatefromjpeg($filename), $filename, $quality);
+            }
+        }
+		// Carichiamo il contenuto dell'immagine compressa in una variabile
+        $image_content = file_get_contents($filename);
+
+        // Convertiamo l'immagine compressa in formato Blob
+        $blob = mysqli_real_escape_string($conn, $image_content);
+
+		new_product($conn,$_POST['titolo'],$_POST['descrizione'],$_POST['prezzo'],
 		$_POST['tipo_prodotto_finito'],$_POST['altezza'],$_POST['larghezza'],
 		$_POST['profondita'],$_POST['spessore'],$_POST['modello'],$_POST['casa_produttrice'],
-		$_POST['indirizzo_magazzino'],$_POST['forma'],$_POST['tipo'],$filename,$_POST['categoria']);
-	
-		unlink($filename); //boh,forse va fatto
-	} else {
-    echo "errore immagine";
-  	}	
+		$_POST['indirizzo_magazzino'],$_POST['forma'],$_POST['tipo'],$_POST['categoria'],$azienda,$blob);
+}
+		}
 ?>
 <!DOCTYPE html>
 <html>
@@ -117,7 +123,7 @@ $conn = db_connect();
 		<input type="text" name="tipo" required><br>
 
         <label for="tipo">Immagine:</label>
-        <input type="file" name="image" required>
+        <input type="file" name="image" accept="image/png, image/jpg, image/jpeg" required>
 
 		<input type="submit" value="Inserisci">
 	</form>
