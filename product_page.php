ea444c7871eb0b77f570abc030127c5d527461bd
nikita <nikita.macreniuc.studenti@isii.it>
2023-03-27 01:13:59 +0200

    inizio lavoro pagina prodotto

---
+++ product_page.php
@@ -0,0 +1,59 @@
+<?php include("include/_header.inc.php") ?>
+<button class="back-button">Back</button>
+<div class="product-desc">
+    <div class="product-image">
+        <img src="assets/img/Table.png" alt="">
+    </div>
+    <div class="product-name">
+        Tavolo da Pranzo TemaHome Nice, White & Oak
+    </div>
+    <div class="product-rating">
+        <img src="assets/img/stars/star_4.png" alt="">
+    </div>
+    <div class="product-price">
+        83,96$
+    </div>
+    <div class="product-specs">
+        <div class="product-specs-key">
+            <ul>
+                <li>Dimensions</li>
+                <li>Color</li>
+                <li>Brand name</li>
+                <li>Category</li>
+                <li>Type</li>
+                <li>Weight</li>
+            </ul>
+        </div>
+        <div class="product-specs-value">
+            <ul>
+                <li>70 x 110 x 73 cm</li>
+                <li>White/Beige</li>
+                <li>TemaHome</li>
+                <li>Kitchen</li>
+                <li>Dining table</li>
+                <li>100Kg</li>
+            </ul>
+        </div>
+    </div>
+    <button class="product-buy">
+        Buy Now
+    </button>
+    <button class="product-add">
+        Add to Cart
+    </button>
+</div>
+<div class="product-about">
+    About this item:
+    <ul>
+        <li>Suitable for small spaces, the Nice table saves space and is excellent in the kitchen or in a small dining room</li>
+        <li>Functional table, which can accommodate 4 adults and a maximum of 6 in the presence of children</li>
+        <li>Modern and contemporary design</li>
+        <li>Made of 16mm thick melamine panels and mostly recycled wood based</li>
+        <li>Delivered as a kit with reinforced packaging and assembly instructions (English language not guaranteed)</li>
+        <li>Use: domestic interior</li>
+        <li>Maintenance: easy to clean with the help of a slightly moistened cloth with water</li>
+        <li>Eco friendly</li>
+        <li>Made in France</li>
+    </ul>
+</div>
+<?php include("include/_footer.inc.php"); ?>
