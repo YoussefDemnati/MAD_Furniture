3cbc3a7484275fa88e87e088a2316db1d3dfe5c6
nikita <nikita.macreniuc.studenti@isii.it>
2023-04-14 09:39:49 +0200

    layout pagina checkout

--- include/_db_config.inc.php
+++ include/_db_config.inc.php
@@ -1,7 +1,7 @@
 
 <?php 
 $servername = "localhost";
-$db_username = "my_madforniture";
+$db_username = "my_madfurniture";
 $db_password = "password.123";
-$db_name = "my_madforniture";
+$db_name = "my_madfurniture";
 ?>
